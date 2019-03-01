68798fb26ab1d780aacc86363d97a779bdcec471
Denis WONG <super2ni@gmail.com>
2019-03-01 14:04:05 +0100

    Offering component specific exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import fabric from '../lib/office-ui-fabric';
 import componentsInstaller from './install';
+import breadcrumbInstaller from './components/breadcrumb';
+import buttonInstaller from './components/button';
+import calloutInstaller from './components/callout';
+import checkboxInstaller from './components/checkbox';
+import choiceFieldGroupInstaller from './components/choice_field_group';
+import commandBarInstaller from './components/command_bar';
+import commandButtonInstaller from './components/command_button';
+import contextualMenuInstaller from './components/contextual_menu';
+import datePickerInstaller from './components/date_picker';
+import dialogInstaller from './components/dialog';
+import dropdownInstaller from './components/dropdown';
+import labelInstaller from './components/label';
+import linkInstaller from './components/link';
+import listInstaller from './components/list';
+import messageBannerInstaller from './components/message_banner';
+import messageBarInstaller from './components/message_bar';
+import orgChartInstaller from './components/org_chart';
+import overlayInstaller from './components/overlay';
+import panelInstaller from './components/panel';
+import personaInstaller from './components/persona';
+import pivotInstaller from './components/pivot';
+import progressIndicatorInstaller from './components/progress_indicator';
+import searchBoxInstaller from './components/search_box';
+import spinnerInstaller from './components/spinner';
+import tableInstaller from './components/table';
+import textFieldInstaller from './components/text_field';
+import toggleInstaller from './components/toggle';
 
 const OfficeUIFabricVue = {
   install(Vue) {
@@ -9,3 +36,167 @@ const OfficeUIFabricVue = {
 };
 
 export default OfficeUIFabricVue;
+
+export const breadcrumbInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    breadcrumbInstaller(Vue);
+  }
+}
+
+export const buttonInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    buttonInstaller(Vue);
+  }
+}
+export const calloutInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    calloutInstaller(Vue);
+  }
+}
+export const checkboxInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    checkboxInstaller(Vue);
+  }
+}
+export const choiceFieldGroupInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    choiceFieldGroupInstaller(Vue);
+  }
+}
+export const commandBarInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    commandBarInstaller(Vue);
+  }
+}
+export const commandButtonInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    commandButtonInstaller(Vue);
+  }
+}
+export const contextualMenuInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    contextualMenuInstaller(Vue);
+  }
+}
+export const datePickerInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    datePickerInstaller(Vue);
+  }
+}
+export const dialogInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    dialogInstaller(Vue);
+  }
+}
+export const dropdownInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    dropdownInstaller(Vue);
+  }
+}
+export const labelInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    labelInstaller(Vue);
+  }
+}
+export const linkInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    linkInstaller(Vue);
+  }
+}
+export const listInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    listInstaller(Vue);
+  }
+}
+export const messageBannerInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    messageBannerInstaller(Vue);
+  }
+}
+export const messageBarInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    messageBarInstaller(Vue);
+  }
+}
+export const orgChartInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    orgChartInstaller(Vue);
+  }
+}
+export const overlayInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    overlayInstaller(Vue);
+  }
+}
+export const panelInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    panelInstaller(Vue);
+  }
+}
+export const personaInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    personaInstaller(Vue);
+  }
+}
+export const pivotInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    pivotInstaller(Vue);
+  }
+}
+export const progressIndicatorInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    progressIndicatorInstaller(Vue);
+  }
+}
+export const searchBoxInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    searchBoxInstaller(Vue);
+  }
+}
+export const spinnerInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    spinnerInstaller(Vue);
+  }
+}
+export const tableInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    tableInstaller(Vue);
+  }
+}
+export const textFieldInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    textFieldInstaller(Vue);
+  }
+}
+export const toggleInstaller = {
+  install(Vue) {
+    Vue.prototype.$fabric = fabric;
+    toggleInstaller(Vue);
+  }
+}
